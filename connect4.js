2d92cef9ee4cf1ea549b48267037c4732a0977e8
sam <wax654@gmail.com>
2021-01-08 00:01:10 -0800

    fixed bug where I referenced the same array for every row

--- connect4.js
+++ connect4.js
@@ -8,7 +8,7 @@
 const WIDTH = 7;
 const HEIGHT = 6;
 
-const currPlayer = 1; // active player: 1 or 2
+let currPlayer = 1; // active player: 1 or 2
 const board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
@@ -16,43 +16,53 @@ const board = []; // array of rows, each row is array of cells  (board[y][x])
  */
 
 function makeBoard() {
-  const row = [];
-  const fillArray = (arr, val, num) => {
-    for (let i = 0; i < num; i++) arr.push(val);
-  }
-  fillArray(row, null, WIDTH);
-  //row should now be WIDTH wide, all null
-
-  fillArray(board, [...row], HEIGHT);
-  //board should now be filled with HEIGHT number of separate rows
-
-
+  repeat(HEIGHT, (y) => {
+    // a row for each row
+    board.push([]);
+    repeat(WIDTH, () => {
+      board[y].push(null);
+    });
+  });
 }
+
+function repeat(iterations, func, ...rest) {
+  for (let i = 0; i < iterations; i++) func(i, ...rest);
+}
+
 
 
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
 function makeHtmlBoard() {
-  // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
+  //  get "htmlBoard" variable from the item in HTML w/ID of "board"
   const htmlBoard = document.getElementById("board");
-  // TODO: add comment for this code
-  var top = document.createElement("tr");
+
+  // creating the top row of the connect four board
+  const top = document.createElement("tr");
   top.setAttribute("id", "column-top");
   top.addEventListener("click", handleClick);
 
-  for (var x = 0; x < WIDTH; x++) {
-    var headCell = document.createElement("td");
+  //creating the individual td's in the top row of the board
+  for (let x = 0; x < WIDTH; x++) {
+    const headCell = document.createElement("td");
+    //Sets each id to the horizontal index it is in
     headCell.setAttribute("id", x);
+    //appends it to the row in order
     top.append(headCell);
   }
+  //appends the whole top row to the board
   htmlBoard.append(top);
 
-  // TODO: add comment for this code
-  for (var y = 0; y < HEIGHT; y++) {
+  // creates the rest of the rows for the board
+  for (let y = 0; y < HEIGHT; y++) {
+    // a row for each row
     const row = document.createElement("tr");
-    for (var x = 0; x < WIDTH; x++) {
+    for (let x = 0; x < WIDTH; x++) {
+      // and a cell for each column on that row
       const cell = document.createElement("td");
+      // each cells id contains it's cordinates of format '3-2' meaning 
+      //    the fourth row from the top, and the third cell from the left
       cell.setAttribute("id", `${y}-${x}`);
       row.append(cell);
     }
@@ -63,14 +73,17 @@ function makeHtmlBoard() {
 /** findSpotForCol: given column x, return top empty y (null if filled) */
 
 function findSpotForCol(x) {
-  // TODO: write the real version of this, rather than always returning 0
-  return 0;
+  // TODO: write the real version of this, rather than always returning 
+  return;
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
 
 function placeInTable(y, x) {
   // TODO: make a div and insert into correct table cell
+  const piece = document.createElement('div');
+  piece.classList.add('piece', 'p' + currPlayer);
+  document.getElementById(`${y}-${x}`).append(piece);
 }
 
 /** endGame: announce game end */
@@ -94,7 +107,7 @@ function handleClick(evt) {
   // place piece in board and add to HTML table
   // TODO: add line to update in-memory board
   placeInTable(y, x);
-
+  board[y][x] = currPlayer;
   // check for win
   if (checkForWin()) {
     return endGame(`Player ${currPlayer} won!`);
@@ -102,9 +115,12 @@ function handleClick(evt) {
 
   // check for tie
   // TODO: check if all cells in board are filled; if so call, call endGame
-
+  if (board.every((row) => row.every((cell) => cell != null))) {
+    endGame();
+  }
   // switch players
   // TODO: switch currPlayer 1 <-> 2
+  currPlayer === 1 ? currPlayer = 2 : currPlayer = 1;
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
